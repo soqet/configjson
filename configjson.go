fdba2ab173aea75eddb20c026bd13812ba27ea94
Gubin Dmitry <soqet@bk.ru>
2022-09-20 16:54:49 +0700

    added license and documentation

--- configjson.go
+++ configjson.go
@@ -1,8 +1,8 @@
 package configjson
 
 import (
-	jsoniter "github.com/json-iterator/go"
 	"bytes"
+	jsoniter "github.com/json-iterator/go"
 	"os"
 )
 
@@ -10,22 +10,27 @@ var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func readFile(path string) ([]byte, error) {
 	configFile, err := os.OpenFile(path, os.O_RDONLY, 0666)
-	if err != nil { return []byte{}, err }
+	if err != nil {
+		return []byte{}, err
+	}
 	defer configFile.Close()
 	buf := bytes.Buffer{}
 	buf.ReadFrom(configFile)
 	return buf.Bytes(), nil
 }
 
-
 func unmarshalConfig(data []byte, config any) error {
 	err := json.Unmarshal(data, config)
 	return err
 }
 
+// ReadConfigFile parses the JSON-encoded file and stores the result in value pointed to by config. 
+// If config is nil or not a pointer, ReadConfigFile returns an InvalidUnmarshalError.
 func ReadConfigFile(path string, config any) error {
 	data, err := readFile(path)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	err = unmarshalConfig(data, config)
 	return err
 }
